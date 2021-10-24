bee25ca77f66e73c71c022c5e1bc28de715558cb
leonnuxy <35940208+leonnuxy@users.noreply.github.com>
2021-10-23 22:57:51 -0600

    Add files via upload

---
+++ count_complete_treenodes.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def getHeight(root):
+            if not root:
+                return 0
+            return 1 + getHeight(root.left)
+        
+        height = getHeight(root)
+        if height == 0:
+            return 0
+        if height == 1:
+            return 1
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+    
+    
