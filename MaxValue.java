ddf5dd8e2fb7d71a7687af167c690e3623d898b9
leonnuxy <35940208+leonnuxy@users.noreply.github.com>
2021-11-21 00:03:28 -0700

    Add files via upload

---
+++ MaxValue.java
@@ -0,0 +1,15 @@
+// Create a class that gets the maximume value of a list of numbers.
+
+
+public class MaxValue {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
